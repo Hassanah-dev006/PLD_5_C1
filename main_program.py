d834155ce79b7b3b11ad5467adb364c2380c3996
BizimaCJ <c.bizima@alustudent.com>
2025-10-26 17:06:33 +0200

    File 4 completion

--- main_program.py
+++ main_program.py
@@ -1,6 +1,7 @@
 from helper_functions import validate_input, convert_to_binary, create_message
 from file_manager import save_message, read_message
-from greetings import show_intro, show_exit_message
+from greetings import show_intro, 
+show_exit_message
 
 
 def get_user_info():
