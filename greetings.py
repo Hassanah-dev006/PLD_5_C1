0426f4bdc4c24e3d04f88400a60dfd2ff3590a80
Kaylain <a.kayitare@alustudent.com>
2025-10-27 01:09:32 -0700

    Program to greet users

---
+++ greetings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def show_intro():
+    print("==================================")
+    print("Welcome to the user info program")
+    print("==================================")
+
+def show_exit_message():
+    print("Thank you for using our program. Goodbye!")
+
